3991823ad1739ab991ef17eb65bd71762ff651ab
Dumitras <dumitrufrimu118@gmail.com>
2025-08-15 01:33:42 +0300

    add about fragment and navbar

--- src/main/java/com/example/skillswap/controller/HomeController.java
+++ src/main/java/com/example/skillswap/controller/HomeController.java
@@ -11,9 +11,44 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/categoryList")
+    @GetMapping("/404")
+    public String error404(){
+        return "404";
+    }
+
+    @GetMapping("/categories-list")
     public String categoryList(){
         return "categories-list";
+    }
+
+    @GetMapping("/categories-grid")
+    public String categoryGrid(){
+        return "categories-grid";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("/details-post-default")
+    public String detailsPostDefault(){
+        return "details-post-default";
+    }
+
+    @GetMapping("/details-post-gallery")
+    public String detailsPostGallery(){
+        return "details-post-gallery";
+    }
+
+    @GetMapping("/details-post-review")
+    public String detailsPostReview(){
+        return "details-post-review";
+    }
+
+    @GetMapping("/typography")
+    public String typography(){
+        return "typography";
     }
 
 }
